5f373d5e9e92aa97f3243c9e29f737f890c8491d
Rhett <rhett.newton@gmail.com>
2017-11-11 09:31:19 -0600

    Add dotnet project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Barker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
