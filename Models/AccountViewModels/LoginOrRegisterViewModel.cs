e4d561cabfb7e8bbd72b8d063ee17697cd5916af
Rhett <rhett.newton@gmail.com>
2017-11-25 09:55:03 -0600

    Split LoginOrRegisterViewModel into two seperate classes

--- Models/AccountViewModels/LoginOrRegisterViewModel.cs
+++ Models/AccountViewModels/LoginOrRegisterViewModel.cs
@@ -8,38 +8,7 @@ namespace Barker.Models.AccountViewModels
 {
     public class LoginOrRegisterViewModel
     {
-        /* Login */
-        [EmailAddress]
-        public string LoginEmail { get; set; }
-
-        [DataType(DataType.Password)]
-        public string LoginPassword { get; set; }
-
-        [Display(Name = "Remember me?")]
-        public bool LoginRememberMe { get; set; }
-
-
-        /* Register */
-        [Display(Name = "Full Name")]
-        [StringLength(50, MinimumLength = 2)]
-        public string RegisterName { get; set; }
-
-        [Display(Name = "User Name")]
-        [StringLength(25, MinimumLength = 3)]
-        public string RegisterUserName { get; set; }
-
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string RegisterEmail { get; set; }
-
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string RegisterPassword { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("RegisterPassword", ErrorMessage = "The password and confirmation password do not match.")]
-        public string RegisterConfirmPassword { get; set; }
+        public LoginViewModel Login { get; set; }
+        public RegisterViewModel Register { get; set; }
     }
 }
