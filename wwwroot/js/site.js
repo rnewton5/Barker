c903cb4fc49eb9e94d010e42bac1f7546f4b166a
Rhett <rhett.newton@gmail.com>
2017-12-11 14:24:25 -0600

    Deleted submitPost.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,6 +1,40 @@
 ﻿function displayMessage(message) {
-    $("#status-message").html(message);
-    $("#status-message").animate({top:'60px'}, 1000);
-    $("#status-message").animate({top:'60px'}, 2000);
-    $("#status-message").animate({top:'-150px'}, 500);
-  }
+  $("#status-message").html(message);
+  $("#status-message").animate({top:'60px'}, 1000);
+  $("#status-message").animate({top:'60px'}, 2000);
+  $("#status-message").animate({top:'-150px'}, 500);
+}
+
+$("#submit-post-button").click(function(event) {
+  event.preventDefault();
+  var $form = $("#submit-post-form"),
+    term = $form.find("textarea[name='Message']").val(),
+    url = $form.attr("action");
+
+  var model = { Message: term };
+
+  var posting = $.post( url, model);
+
+  posting.done(function(data) {
+    displayMessage(data.message);
+    $('#barkModal').modal( 'hide' );
+    $('.bark-textarea').val('');
+  })
+})
+
+$("#modal-submit-btn").click(function(event) {
+  event.preventDefault();
+  var $form = $("#modal-submit-post-form"),
+  term = $form.find("textarea[name='Message']").val(),
+  url = $form.attr("action");
+
+  var model = { Message: term };
+
+  var posting = $.post( url, model);
+
+  posting.done(function(data) {
+    displayMessage(data.message);
+    $('#barkModal').modal( 'hide' );
+    $('.modal-textarea').val('');
+  })
+})
