4d98dee351b383df8d3c3366890ffccead6bae73
Rhett <rhett.newton@gmail.com>
2017-12-11 10:15:43 -0600

    Fix submit post bug

--- wwwroot/js/submitPost.js
+++ wwwroot/js/submitPost.js
@@ -1,8 +1,24 @@
-$(".submit-post-form").click(function(event) {
+$("#submit-post-button").click(function(event) {
     event.preventDefault();
-    var $form = $(this),
+    var $form = $("#submit-post-form"),
         term = $form.find("textarea[name='Message']").val(),
         url = $form.attr("action");
+
+    var model = { Message: term };
+
+    var posting = $.post( url, model);
+
+    posting.done(function(data) {
+        displayMessage(data.message);
+        $('#barkModal').modal( 'hide' );
+    })
+})
+
+$("#modal-submit-btn").click(function(event) {
+    event.preventDefault();
+    var $form = $("#modal-submit-post-form"),
+    term = $form.find("textarea[name='Message']").val(),
+    url = $form.attr("action");
 
     var model = { Message: term };
 
