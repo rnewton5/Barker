03daf18f60581d05b9afebc6854e77a73e3cfee0
Rhett <rhett.newton@gmail.com>
2017-12-18 11:55:50 -0600

    Add inheritance to User profile view models

--- Models/UserViewModels/BaseProfileViewModel.cs
+++ Models/UserViewModels/BaseProfileViewModel.cs
@@ -7,16 +7,17 @@ using Barker.Models.PostViewModels;
 
 namespace Barker.Models.UserViewModels
 {
-    public class FollowersViewModel : BaseProfileViewModel
-    {
-        public ICollection<FollowerUser> FollowerUsers { get; set; }
-    }
-
-    public class FollowerUser 
+    public class BaseProfileViewModel
     {
         public string UserName { get; set; }
         public int BarksCount { get; set; }
+        public int FollowingCount { get; set; }
         public int FollowersCount { get; set; }
-        public bool isFollowing { get; set; }
+        public int LikesCount { get; set; }
+        public DateTime JoinDate { get; set; }
+        public ICollection<string> Following { get; set; }
+        public ICollection<string> OtherUsers { get; set; }
+
+        public PostViewModel PostVm { get; set; }
     }
 }
--- Models/UserViewModels/ProfileViewModel.cs
+++ Models/UserViewModels/ProfileViewModel.cs
@@ -7,17 +7,6 @@ using Barker.Models.PostViewModels;
 
 namespace Barker.Models.UserViewModels
 {
-    public class ProfileViewModel
-    {
-        public string UserName { get; set; }
-        public int BarksCount { get; set; }
-        public int FollowingCount { get; set; }
-        public int FollowersCount { get; set; }
-        public int LikesCount { get; set; }
-        public DateTime JoinDate { get; set; }
-        public ICollection<string> Following { get; set; }
-        public ICollection<string> OtherUsers { get; set; }
-
-        public PostViewModel PostVm { get; set; }
-    }
+    public class ProfileViewModel : BaseProfileViewModel
+    { }
 }
