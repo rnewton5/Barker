14fcf0de2525a91fa6339c9e9874930dcbcab775
Rhett <rhett.newton@gmail.com>
2017-12-19 09:50:46 -0600

    Add 'Key' data annotation to Id property

--- Models/Image.cs
+++ Models/Image.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Barker.Models
 {
     public class Image
     {
+        [Key]
         public Guid Id { get; set; }
 
         public string Name { get; set; }
