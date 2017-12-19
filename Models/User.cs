8ba53d33fb21bac4b1ab17c10340d12cef90ec8d
Rhett <rhett.newton@gmail.com>
2017-12-19 09:51:42 -0600

    Add 'ProfileImageId' property to User model

--- Models/User.cs
+++ Models/User.cs
@@ -14,6 +14,7 @@ namespace Barker.Models
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime JoinDate { get; set; }
+        public Guid ProfileImageId { get; set; }
 
         // Navigation properties
         public List<Post> Posts { get; set; }
