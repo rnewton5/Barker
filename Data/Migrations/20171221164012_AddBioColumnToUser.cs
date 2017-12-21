21acc3b6d3c2f921f5e2fb14109370d1b7106fba
Rhett <rhett.newton@gmail.com>
2017-12-21 10:41:22 -0600

    Create 'AddBioColumnToUser' migration

---
+++ Data/Migrations/20171221164012_AddBioColumnToUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Barker.Migrations
+{
+    public partial class AddBioColumnToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "AspNetUsers",
+                type: "varchar(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "AspNetUsers");
+        }
+    }
+}
